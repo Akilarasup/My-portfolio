5228301a23283e2176ce1911680d3324720f3f97
Akilarasu P <akilarasup2000@gmail.com>
2024-06-19 14:47:00 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import { Container } from 'react-bootstrap';
 const Footer = () => {
   return (
     <footer className="bg-dark py-4 mt-4">
-      <Container className="text-center text-muted">
+      <Container className="text-center text-light"> {/* Changed text-muted to text-light */}
         <p>&copy; {new Date().getFullYear()} Your Name. All rights reserved.</p>
       </Container>
     </footer>
