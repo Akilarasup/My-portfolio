2ed11b1b337e9c638d0b3a64a799d40db7524a17
Akilarasu P <akilarasup2000@gmail.com>
2024-06-26 13:10:01 +0530

    Removed projects page and integrated with experience page

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,6 @@ const Header = () => {
             <Nav.Link as={Link} to="/about" className="nav-link me-3">About</Nav.Link>
             <Nav.Link as={Link} to="/skills" className="nav-link me-3">Skills</Nav.Link>
             <Nav.Link as={Link} to="/experience" className="nav-link me-3">Experience</Nav.Link>
-            <Nav.Link as={Link} to="/projects" className="nav-link me-3">Projects</Nav.Link>
             <Nav.Link as={Link} to="/contact" className="nav-link me-3">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,6 @@ const MyNavbar = () => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
             <Nav.Link as={Link} to="/about">About</Nav.Link>
             <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
             <Nav.Link as={Link} to="/skills">Skills</Nav.Link>
