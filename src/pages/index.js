57336e81d42df04ca5ea67035a2d52a7cf984a83
Akilarasu P <akilarasup2000@gmail.com>
2024-07-03 11:44:39 +0530

    index page alignment

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,30 @@
 import React from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
-import { Link } from 'gatsby'; // Import Link from Gatsby
+import { Link } from 'gatsby';
 import Layout from '../components/Layout';
-import heroBackground from '../images/pexels-photo-2680270.webp'; // Ensure correct import path
-import './index.css'; // Import the CSS file for specific styles
+import heroBackground from '../images/pexels-photo-2680270.webp';
+import './index.css';
 
 const IndexPage = () => {
   return (
     <Layout>
-      {/* Hero Section */}
       <div className="hero-section" style={{ backgroundImage: `url(${heroBackground})` }}>
-        <div className="container position-relative py-5 d-flex flex-column align-items-center justify-content-center">
-          <StaticImage
-            src="../images/akil.jpg" // Path to Akil's photo
-            alt="Akil's photo"
-            placeholder="blurred"
-            layout="fixed"
-            width={300} // Adjust width based on your design needs
-            height={300} // Adjust height based on your design needs
-            className="img-fluid rounded-circle shadow-lg"
-            style={{ objectFit: 'cover' }} // Ensure the image covers the container
-          />
-          <h1 className="display-4 fw-bold">Hello, I'm Akil</h1>
-          <p className="lead">Improving processes with analytical expertise.</p>
-          
-          {/* Modified button to navigate to About page */}
-          <Link to="/about" className="btn btn-primary">Learn About Me</Link>
-          
+        <div className="container position-relative py-5">
+          <div className="d-flex flex-column align-items-center justify-content-center content-wrapper">
+            <StaticImage
+              src="../images/akil.jpg"
+              alt="Akil's photo"
+              placeholder="blurred"
+              layout="fixed"
+              width={300}
+              height={300}
+              className="img-fluid rounded-circle shadow-lg"
+              style={{ objectFit: 'cover' }}
+            />
+            <h1 className="display-4 fw-bold">Hello, I'm Akil</h1>
+            <p className="lead">Improving processes with analytical expertise.</p>
+            <Link to="/about" className="btn btn-primary">Learn About Me</Link>
+          </div>
         </div>
       </div>
     </Layout>
